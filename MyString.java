38a9bdaa4a8be37f9bcb7d044538f953c64d49fb
benalt123 <benben96363@gmail.com>
2024-12-12 23:35:53 +0200

    partially

--- MyString.java
+++ MyString.java
@@ -9,6 +9,7 @@ public class MyString {
         System.out.println(countChar(hello, 'z'));
         System.out.println(spacedString(hello));
         //// Put your other tests here.
+        
     }
 
     /**
@@ -20,8 +21,11 @@ public class MyString {
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++)
+            if(str.charAt(i) == ch)
+                counter++;
+        return counter;
     }
 
     /** Returns true if str1 is a subset string str2, false otherwise
@@ -36,8 +40,10 @@ public class MyString {
      * @return true is str1 is a subset of str2, false otherwise
      */
     public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
-        return false;
+        for(int i = 0; i < str1.length() ; i++)
+            if(countChar(str1,str1.charAt(i)) > countChar(str2,str1.charAt(i)))
+                return false;
+        return true;
     }
 
     /** Returns a string which is the same as the given string, with a space
@@ -49,8 +55,14 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+        String news = "";
+        if(!str.isEmpty()){
+            news = String.valueOf(str.charAt(0));
+            for(int i = 1; i < str.length(); i++)
+                news += " " + str.charAt(i);
+        }
+         
+        return news;
     }
   
     /**
@@ -64,8 +76,10 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
+        String random = "";
+        for (int i = 0; i < n; i++)
+            random += (char) (97 + Math.random()* 26);
+        return random;
     }
 
     /**
@@ -78,9 +92,16 @@ public class MyString {
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
+        
+        for (int i = 0; i < str2.length(); i++){
+            int place = str1.indexOf(str2.charAt(i));
+            str1 = str1.substring(0,place) + str1.substring(place + 1, str1.length());
+            
+        }   
+        return str1;
     }
+    
+    
 
     /**
      * Returns a string consisting of the given string, with the given 
--- Scrabble.java
+++ Scrabble.java
@@ -48,7 +48,9 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		//// Replace the following statement with your code
+		for (int i = 0; i < NUM_OF_WORDS; i++) {
+			if (DICTIONARY[i].equals(word)) return true;
+		}
 		return false;
 	}
 	
@@ -56,16 +58,29 @@ public class Scrabble {
 	// If the length of the word equals the length of the hand, adds 50 points to the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
-		//// Replace the following statement with your code
-		return 0;
+		int score = 0;
+		for (int i = 0; i < word.length(); i++) {
+			score += SCRABBLE_LETTER_VALUES[word.charAt(i) - 'a'];
+		}
+		score = score * word.length();
+		if (word.length() == HAND_SIZE) {
+			score += 50;
+		}
+		if (MyString.subsetOf("runi", word)) {
+			score += 1000;
+		}
+		return score;
+		
 	}
 
 	// Creates a random hand of length (HAND_SIZE - 2) and then inserts
 	// into it, at random indexes, the letters 'a' and 'e'
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
-		//// Replace the following statement with your code
-		return null;
+		String hand = MyString.randomStringOfLetters(HAND_SIZE - 2);
+		hand = MyString.insertRandomly('a', hand);
+		hand = MyString.insertRandomly('e', hand);
+		return hand;
 	}
 	
     // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
